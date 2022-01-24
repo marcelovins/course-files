a5b5f1b4b97c1625de05824632af1304c5202fac
marcelovins <97124032+marcelovins@users.noreply.github.com>
2022-01-24 17:02:56 -0300

    Curso Gilberto Toledo
    Inclusão dos arquivos do curso

--- Exercicio 4_lista 05/main.cpp
+++ Exercicio 4_lista 05/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string>
+#include <conio.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string nome[50];
+    int B;
+    for(B=0;B<4;B++)
+    {
+        printf("\ndigite o nome:\n");
+        scanf("%s", &nome[B]);
+        printf("\n\nNomes: %s\n\n",nome[B]);
+    }
     return 0;
 }
